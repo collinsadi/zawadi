38253afad54fb2695b8c052acc9700108819d085
collinsadi <collinsadi20@gmail.com>
2025-09-22 22:19:09 +0100

    feat: created separate error and library files

---
+++ contract/contracts/Libraries/EscrowLib.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+
+//library for escrow related structs
+library EscrowLib {
+   
+}
--- contract/contracts/Zawadi/Escrow.sol
+++ contract/contracts/Zawadi/Escrow.sol
@@ -4,9 +4,13 @@ pragma solidity ^0.8.28;
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {ReentrancyGuard} from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "../Errors/EscrowErrors.sol";
 
 
 contract Escrow {
+
+// using safe ERC20 for IERC20 so we can use the safe transfer functions
+using SafeERC20 for IERC20;
 
 }
 
