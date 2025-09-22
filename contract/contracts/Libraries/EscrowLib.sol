1a161af6274d66d49aaacf9618c892ee7bd9807e
collinsadi <collinsadi20@gmail.com>
2025-09-22 22:22:12 +0100

    feat: created some structs needed for the escrow contract

--- contract/contracts/Libraries/EscrowLib.sol
+++ contract/contracts/Libraries/EscrowLib.sol
@@ -1,8 +1,27 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.28;
 
-
 //library for escrow related structs
 library EscrowLib {
-   
+    struct Challenge {
+        uint256 totalPrize;
+        address sponsor;
+        bool isPaidOut;
+        address token;
+        bool isERC20;
+        string ipfsCid;
+        bool isFunded;
+    }
+
+    struct Allocation {
+        uint256 position;
+        uint256 amount;
+        address winner;
+        bool claimed;
+    }
+
+    struct Approval {
+        bool sponsorApproved;
+        bool organiserApproved;
+    }
 }
