95686e56f8825819f91062d8a3718029784c1cd3
collinsadi <collinsadi20@gmail.com>
2025-09-22 22:30:54 +0100

    added a separate file to handle all events

---
+++ contract/contracts/Events/EscrowEvents.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+//events for the escrow contract
+event ChallengeAdded(uint256 indexed challengeId, address sponsor, uint256 totalPrize, string ipfsCid);
+event ChallengeFunded(uint256 indexed challengeId, address sponsor, uint256 amount);
+event WinnersAdded(uint256 indexed challengeId, address[] winners, uint256[] allocations);
+event DistributionApproved(uint256 indexed challengeId, address approver);
+event PrizeClaimed(uint256 indexed challengeId, address winner, uint256 amount);
+event ConfigurationLocked();
--- contract/contracts/Zawadi/Escrow.sol
+++ contract/contracts/Zawadi/Escrow.sol
@@ -6,12 +6,24 @@ import {ReentrancyGuard} from "@openzeppelin/contracts/utils/ReentrancyGuard.sol
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "../Errors/EscrowErrors.sol";
 import "../Libraries/EscrowLib.sol";
+import "../Events/EscrowEvents.sol";
 
 
 contract Escrow {
 
 // using safe ERC20 for IERC20 so we can use the safe transfer functions
 using SafeERC20 for IERC20;
+
+
+// state variables
+address public organizer;
+bool public isLocked;
+uint256 public challengeCount;
+
+// mappings
+mapping(uint256 => EscrowLib.Challenge) public challenges;
+mapping(uint256 => EscrowLib.Allocation) public allocations;
+mapping(uint256 => EscrowLib.Approval) public approvals;
 
 }
 
