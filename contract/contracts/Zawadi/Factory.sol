1e592b3459d50c6efc72fde2f02f5c4813642dc7
collinsadi <collinsadi20@gmail.com>
2025-09-23 00:54:26 +0100

    feat: added detailed comments to the factory contract

--- contract/contracts/Zawadi/Factory.sol
+++ contract/contracts/Zawadi/Factory.sol
@@ -6,26 +6,45 @@ import "../Libraries/FactoryLib.sol";
 import "../Errors/FactoryError.sol";
 import "../Events/FactoryEvents.sol";
 
+/**
+ * @title Factory
+ * @dev Factory contract for creating and managing hackathon escrow contracts
+ * @notice This contract allows organizers to create hackathons and deploy associated escrow contracts
+ */
 contract Factory {
+    /// @notice Address of the factory owner who can transfer ownership
     address public factoryOwner;
-
-    // Array to track all hackathons
+    
+    /// @notice Array containing all created hackathons for iteration and counting
     FactoryLib.Hackathon[] public allHackathons;
-
-    // Mapping to track hackathons by ID
+    
+    /// @notice Mapping from hackathon ID to hackathon details for efficient lookups
     mapping(bytes32 => FactoryLib.Hackathon) public hackathons;
 
+    /**
+     * @dev Constructor sets the deployer as the initial factory owner
+     */
     constructor() {
         factoryOwner = msg.sender;
     }
 
     // Modifiers
+    
+    /**
+     * @dev Modifier that restricts access to the factory owner only
+     * @notice Reverts if the caller is not the factory owner
+     */
     modifier onlyFactoryOwner() {
         if (msg.sender != factoryOwner)
             revert Factory__OnlyFactoryOwnerCanAccess();
         _;
     }
 
+    /**
+     * @dev Modifier that restricts access to the organizer of a specific hackathon
+     * @param _id The unique identifier of the hackathon
+     * @notice Reverts if the hackathon doesn't exist or caller is not the organizer
+     */
     modifier onlyOrganizer(bytes32 _id) {
         FactoryLib.Hackathon memory hackathon = hackathons[_id];
         if (hackathon.organizer == address(0))
@@ -38,7 +57,14 @@ contract Factory {
 
     // Events are imported from FactoryEvents.sol
 
-    // Function to create a new hackathon
+    /**
+     * @notice Creates a new hackathon and deploys an associated escrow contract
+     * @dev Generates a unique hackathon ID using organizer address, IPFS CID, and timestamp
+     * @param _ipfsCid The IPFS content identifier containing hackathon details
+     * @return hackathonId The unique identifier for the created hackathon
+     * @return escrowContract The address of the deployed escrow contract
+     * @notice The caller becomes the organizer of the hackathon
+     */
     function createHackathon(
         string memory _ipfsCid
     ) external returns (bytes32 hackathonId, address escrowContract) {
@@ -75,7 +101,13 @@ contract Factory {
         return (hackathonId, escrowContract);
     }
 
-    // Function to get hackathon by ID
+    /**
+     * @notice Retrieves hackathon details by its unique identifier
+     * @dev Reverts if the hackathon doesn't exist
+     * @param _hackathonId The unique identifier of the hackathon to retrieve
+     * @return The hackathon struct containing all details
+     * @notice Reverts with Factory__HackathonDoesNotExist if hackathon is not found
+     */
     function getHackathonById(
         bytes32 _hackathonId
     ) external view returns (FactoryLib.Hackathon memory) {
@@ -86,7 +118,12 @@ contract Factory {
         return hackathon;
     }
 
-    // Function to get all hackathons
+    /**
+     * @notice Retrieves all created hackathons
+     * @dev Returns the complete array of all hackathons
+     * @return Array of all hackathon structs
+     * @notice This function can be gas-intensive for large numbers of hackathons
+     */
     function getAllHackathons()
         external
         view
@@ -95,12 +132,22 @@ contract Factory {
         return allHackathons;
     }
 
-    // Function to get hackathon count
+    /**
+     * @notice Returns the total number of created hackathons
+     * @dev Returns the length of the allHackathons array
+     * @return The total count of hackathons
+     */
     function getHackathonCount() external view returns (uint256) {
         return allHackathons.length;
     }
 
-    // Function to transfer factory ownership
+    /**
+     * @notice Transfers ownership of the factory to a new address
+     * @dev Can only be called by the current factory owner
+     * @param _newOwner The address to transfer ownership to
+     * @notice Reverts if the new owner address is zero address
+     * @notice Emits OwnershipTransferred event upon successful transfer
+     */
     function transferOwnership(address _newOwner) external onlyFactoryOwner {
         if (_newOwner == address(0)) revert Factory__InvalidAddress();
         address previousOwner = factoryOwner;
