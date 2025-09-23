0c07905ba664ae9ddbe9c57147b7848533e20edd
collinsadi <collinsadi20@gmail.com>
2025-09-23 01:14:18 +0100

    change: moved the test file one step back

--- contract/test/Factory.t.sol
+++ contract/test/Factory.t.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.28;
 
-import {Factory} from "../../contracts/Zawadi/Factory.sol";
-import {FactoryLib} from "../../contracts/Libraries/FactoryLib.sol";
+import {Factory} from "../contracts/Zawadi/Factory.sol";
+import {FactoryLib} from "../contracts/Libraries/FactoryLib.sol";
 import {Test} from "forge-std/Test.sol";
 import {console} from "forge-std/console.sol";
 
