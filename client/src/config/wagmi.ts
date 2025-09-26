83855ff8a8d6d88f31995cadc56597fb4c5d548b
collinsadi <collinsadi20@gmail.com>
2025-09-26 18:47:51 +0100

    changed network to sepolia

--- client/src/config/wagmi.ts
+++ client/src/config/wagmi.ts
@@ -1,9 +1,10 @@
 import { getDefaultConfig } from "@rainbow-me/rainbowkit";
-import { hardhat } from "../chains/hardhat";
+import { sepolia } from "wagmi/chains";
+// import { sepolia } from "../chains/hardhat";
 
 export const config = getDefaultConfig({
   appName: "Zawadi",
   projectId: "81a9762e7ae04a1d2e6bd68b13403653",
-  chains: [hardhat],
+  chains: [sepolia],
   ssr: true,
 });
